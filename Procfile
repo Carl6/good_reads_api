2496d529b3c74f07ae5fab9b147f086706535891
Carla Aracely Mancillas Serna <carlaaracelymancillasserna@MacBook-Pro-de-Carla.local>
2017-07-26 19:06:38 -0500

    almost ready for deploy

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn goodreads_api.wsgi
+
--- goodreads_api/settings/production.py
+++ goodreads_api/settings/production.py
@@ -1,7 +1,13 @@
 from .base import *
+import dj_database_url
+import os
+
 
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
 DATABASES = dict()
+DATABASES['default'] = dj_database_url.config()
+
+STATIC_ROOT = os.path.join(os.getcwd(),'static')
